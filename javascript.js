441c4f840589a9d7a10549104f6884e926abf224
JaviPeder~ <javierpedernera@gmail.com>
2024-04-23 10:23:13 -0300

    git ignore

--- javascript.js
+++ javascript.js
@@ -1,38 +1,48 @@
 const apiKey = window.env.apiKey;
 
 console.log(apiKey);
-fetch(`https://www.googleapis.com/books/v1/volumes?q=war&filter=free-ebooks&maxResults=3&key=${apiKey}`)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    const booksContainer = document.getElementById('booksContainer');
-
-    // Convertir el objeto en un array utilizando Object.values()
-    const booksArray = Object.values(data);
-
-    // Iterar sobre los libros en el array
-    booksArray.forEach(book => {
-      const card = document.createElement('div');
-      card.classList.add('col-md-4', 'mb-4');
-      card.innerHTML = `
-        <div class="card">
-          <img src="${book.cover}" class="card-img-top" alt="${book.title}">
-          <div class="card-body">
-            <h5 class="card-title">${book.title}</h5>
-            <p class="card-text">${book.description}</p>
-            <button class="btn btn-primary addToFavorites">Agregar a Favoritos</button>
-          </div>
-        </div>
-      `;
-      const addToFavoritesBtn = card.querySelector('.addToFavorites');
-      addToFavoritesBtn.addEventListener('click', () => addToFavorites(book));
-
-      booksContainer.appendChild(card);
-    });
-  })
-  .catch(error => console.error('Error fetching books:', error));
-
-// Función para agregar un libro a la lista de favoritos
-function addToFavorites(book) {
-  console.log('Agregado a favoritos:', book);
-}
+document.getElementById('searchButton').addEventListener('click', () => {
+    const searchInput = document.getElementById('searchInput').value.trim();
+    if (searchInput !== '') {
+      searchBooks(searchInput);
+    }
+  });
+  
+  function searchBooks(searchQuery) {
+    const apiKey = window.env.apiKey;
+    const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${searchQuery}&filter=free-ebooks&maxResults=6&key=${apiKey}`;
+  
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        const booksContainer = document.getElementById('booksContainer');
+        booksContainer.innerHTML = ''; // Limpiar el contenedor antes de agregar nuevos libros
+  
+        data.items.forEach((book, index) => {
+          const bookInfo = book.volumeInfo;
+          const cardId = book.id;
+          const card = document.createElement('div');
+          card.id = cardId;
+          card.classList.add('col-md-4', 'mb-4');
+          card.innerHTML = `
+            <div class="card">
+              <img src="${bookInfo.imageLinks?.smallThumbnail || 'https://via.placeholder.com/150'}" class="card-img-top" alt="${bookInfo.title}">
+              <div class="card-body">
+                <h5 class="card-title">${bookInfo.title}</h5>
+                <h6 class="card-title">${bookInfo.subtitle || ''}</h6>
+                <p class="card-text">${bookInfo.authors?.[0] || 'Autor desconocido'}</p>
+                <button class="btn btn-primary addToFavorites">Agregar a Favoritos</button>
+                <button class="btn btn-primary showDescription" data-target="${cardId}">Ver Descripción</button>
+              </div>
+            </div>
+          `;
+    
+          const addToFavoritesBtn = card.querySelector('.addToFavorites');
+          addToFavoritesBtn.addEventListener('click', () => addToFavorites(book));
+    
+          booksContainer.appendChild(card);
+        });
+      })
+      .catch(error => console.error('Error fetching books:', error));
+  }
